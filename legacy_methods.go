64b6364b38794388a32bfd2de847e3aa3047e693
mtavano <chelotavano.udp@gmail.com>
2025-02-27 21:52:23 -0300

    feat: add dropped legacy method

---
+++ legacy_methods.go
@@ -0,0 +1,104 @@
+package logsnag
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// TODO: mofify me when need insight
+func (logsnag *LogSnag) Insight(title string, value string, icon string) bool {
+	url := "https://api.logsnag.com/v1/insight"
+	method := "POST"
+
+	payload := strings.NewReader(`{
+		"project": "` + logsnag.GetProject() + `",
+		"title": "` + title + `",
+		"value": "` + value + `",
+		"icon": "` + icon + `"
+	}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	req.Header.Add("Authorization", "Bearer "+logsnag.Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	_, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	return true
+}
+
+func (logsnag *LogSnag) PublishLegacy(
+	channel string,
+	event string,
+	icon string,
+	tags map[string]any,
+	notify bool,
+) bool {
+	url := "https://api.logsnag.com/v1/log"
+	method := "POST"
+
+	// Create the description string from map
+	var pairs []string
+	for key, value := range tags {
+		pairs = append(pairs, fmt.Sprintf(`%s: %v`, key, value))
+	}
+
+	description := strings.Join(pairs, ", ")
+
+	rawPayload := `{
+		"project": "` + logsnag.GetProject() + `",
+		"channel": "` + channel + `",
+		"event": "` + event + `",
+		"description": "` + description + `",
+		"icon": "` + icon + `",
+		"notify": "` + strconv.FormatBool(notify) + `"
+	}`
+
+	payload := strings.NewReader(rawPayload)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	req.Header.Add("Authorization", "Bearer "+logsnag.Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	_, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	return true
+}
--- logsnag.go
+++ logsnag.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -52,7 +51,10 @@ func (logsnag *LogSnag) Publish(input *PublishRequest) error {
 		return errors.Wrap(err, "logsnag: LogSnag.Publish http.NewRequest error")
 	}
 
-	req.Header.Add("Authorization", "Bearer "+logsnag.Token)
+	req.Header.Add(
+		"Authorization",
+		fmt.Sprintf("Bearer %s", logsnag.Token),
+	)
 	req.Header.Add("Content-Type", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
@@ -70,60 +72,9 @@ func (logsnag *LogSnag) Publish(input *PublishRequest) error {
 	return nil
 }
 
-// TODO: mofify me when need insight
-func (logsnag *LogSnag) Insight(title string, value string, icon string) bool {
-	url := "https://api.logsnag.com/v1/insight"
-	method := "POST"
-
-	payload := strings.NewReader(`{
-		"project": "` + logsnag.GetProject() + `",
-		"title": "` + title + `",
-		"value": "` + value + `",
-		"icon": "` + icon + `"
-	}`)
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, payload)
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	req.Header.Add("Authorization", "Bearer "+logsnag.Token)
-	req.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-	defer res.Body.Close()
-
-	_, err = io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	return true
-}
-
 func NewLogSnag(token string, project string) LogSnag {
 	return LogSnag{
 		Token:   token,
 		Project: project,
 	}
-}
-
-func main() {
-	logSnag := NewLogSnag(
-		"d67d3443e793dad29d9c94df76838367",
-		"ferry-times",
-	)
-
-	logSnag.Insight(
-		"waitlist",    // Channel
-		"User Joined", // Event
-		"🛥️",          // Icon
-	)
 }
